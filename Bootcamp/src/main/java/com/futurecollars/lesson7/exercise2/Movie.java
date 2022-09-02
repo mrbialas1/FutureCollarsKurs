e8909ab498325296e8be10126456d5659f5f145d
Piotr Białas <mrbialas123@gmail.com>
2022-09-02 17:31:57 +0200

    Zadanie 2 z OOP cz. 1

---
+++ Bootcamp/src/main/java/com/futurecollars/lesson7/exercise2/Movie.java
@@ -0,0 +1,14 @@
+package com.futurecollars.lesson7.exercise2;
+
+public class Movie {
+    String theme;
+    String directorName;
+    int movieDuration;
+    int yearOfProduction;
+    public Movie(String theme, String directorName, int movieDuration, int yearOfProduction){
+        this.theme = theme;
+        this.directorName = directorName;
+        this.movieDuration = movieDuration;
+        this.yearOfProduction = yearOfProduction;
+    }
+}
