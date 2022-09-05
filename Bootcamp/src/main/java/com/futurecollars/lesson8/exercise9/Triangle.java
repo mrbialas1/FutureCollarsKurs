b72b2a9232a1e069a55f959874acd6f5bbb9cce9
Piotr Białas <mrbialas123@gmail.com>
2022-09-05 14:22:06 +0200

    Zadanie 9 z OOP cz. 2

---
+++ Bootcamp/src/main/java/com/futurecollars/lesson8/exercise9/Triangle.java
@@ -0,0 +1,14 @@
+package com.futurecollars.lesson8.exercise9;
+
+public class Triangle {
+    int width;
+    int height;
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
