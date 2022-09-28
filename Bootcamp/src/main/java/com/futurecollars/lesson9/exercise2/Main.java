73a7e5c1a9ffc8483609a86818098f92543dc5df
Piotr Białas <mrbialas123@gmail.com>
2022-09-28 23:51:54 +0200

    Exercise 2 - Clean Code: Poprawa

--- Bootcamp/src/main/java/com/futurecollars/lesson9/exercise2/Main.java
+++ Bootcamp/src/main/java/com/futurecollars/lesson9/exercise2/Main.java
@@ -2,21 +2,33 @@ package com.futurecollars.lesson9.exercise2;
 
 public class Main {
     public static void main(String[] args) {
-        for (int number = 1; number <= 100 ; number++) {
-            if(number % 3 == 0){
-                if (number % 5 == 0){
-                    System.out.println("FizzBuzz");
-                }
-                else {
-                    System.out.println("Fizz");
-                }
-            }
-            else if(number % 5 == 0){
-                System.out.println("Buzz");
-            }
-            else{
-                System.out.println(number);
-            }
+        for (int number = 1; number <= 100; number++) {
+            processFizzBuzzGame(number);
+        }
+    }
+
+    private static void processFizzBuzzGame(int number) {
+        writeFizz(number);
+        writeBuzz(number);
+        writeNumber(number);
+        System.out.println();
+    }
+
+    private static void writeNumber(int number) {
+        if (number % 5 != 0 && number % 3 != 0) {
+            System.out.print(number);
+        }
+    }
+
+    private static void writeBuzz(int number) {
+        if (number % 5 == 0) {
+            System.out.print("Buzz");
+        }
+    }
+
+    private static void writeFizz(int number) {
+        if (number % 3 == 0) {
+            System.out.print("Fizz");
         }
     }
 }
