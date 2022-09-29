f98e538ea85341759fa28c74bd1977289973f251
Piotr Białas <mrbialas123@gmail.com>
2022-09-29 19:01:04 +0200

    Exercise 2 - Files, Exceptions: Poprawione

--- Bootcamp/src/main/java/com/futurecollars/lesson10/exercise2/Main.java
+++ Bootcamp/src/main/java/com/futurecollars/lesson10/exercise2/Main.java
@@ -1,18 +1,11 @@
 package com.futurecollars.lesson10.exercise2;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import static com.futurecollars.lesson10.exercise2.FileReader.readFile;
 
 public class Main {
     public static void main(String[] args) {
-        String content = null;
-        try {
-            content = Files.readString(Paths.get("C:/Users/mrbia/Desktop/FutureCollarsKurs/Bootcamp/src/main" +
-                    "/java/com/futurecollars/lesson10/exercise2/data.txt"));
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-        System.out.println(content);
+        String path = "C:/Users/mrbia/Desktop/FutureCollarsKurs/Bootcamp/src/main" +
+                "/java/com/futurecollars/lesson10/exercise2/data.txt";
+        readFile(path);
     }
 }
