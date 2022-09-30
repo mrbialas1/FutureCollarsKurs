6d7ed43f48768fe08be7faf63c053712dead6871
Piotr Białas <mrbialas123@gmail.com>
2022-09-30 13:58:30 +0200

    Exercise 5 - Files, Exceptions: Poprawione

--- Bootcamp/src/main/java/com/futurecollars/lesson9/task5/Car.java
+++ Bootcamp/src/main/java/com/futurecollars/lesson9/task5/Car.java
@@ -1,4 +1,30 @@
 package com.futurecollars.lesson9.task5;
 
-public class Car extends Vehicle {
+public abstract class Car extends Vehicle implements Engine{
+    private boolean engineStarted;
+
+    public Car(boolean engineStarted) {
+        this.engineStarted = engineStarted;
+    }
+
+    @Override
+    public void startEngine() {
+        if (!engineStarted) {
+            engineStarted = true;
+        }
+    }
+    @Override
+    public void stopEngine() {
+        if (engineStarted) {
+            engineStarted = false;
+        }
+    }
+
+    public boolean isEngineStarted() {
+        return engineStarted;
+    }
+    public void setEngineStarted(boolean engineStarted) {
+        this.engineStarted = engineStarted;
+    }
+
 }
