d92120ff7f6964c042ba5bddd9367ea302e0a027
Piotr Białas <mrbialas123@gmail.com>
2022-10-01 10:15:56 +0200

    Exercise 9 - Files, Exceptions

---
+++ Bootcamp/src/main/java/com/futurecollars/lesson10/exercise9/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.futurecollars.lesson10.exercise9;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
---
+++ Bootcamp/src/main/java/com/futurecollars/lesson10/exercise9/Main.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lesson10.exercise9;
+
+public class Main {
+    public static void main(String[] args) {
+        String pesel = "12345678900";
+        PeselValidator.validatePesel(pesel);
+    }
+}
