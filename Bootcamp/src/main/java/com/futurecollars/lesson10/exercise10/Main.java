1d0b0b37f36c55763e3be5abded3cab83dcf73cb
Piotr Białas <mrbialas123@gmail.com>
2022-10-01 10:16:39 +0200

    Exercise 10 - Files, Exceptions: Formatowanie

--- Bootcamp/src/main/java/com/futurecollars/lesson10/exercise10/Main.java
+++ Bootcamp/src/main/java/com/futurecollars/lesson10/exercise10/Main.java
@@ -1,8 +1,8 @@
 package com.futurecollars.lesson10.exercise10;
 
-public class Main extends ByThreeDivisibilityValidator{
+public class Main extends ByThreeDivisibilityValidator {
     public static void main(String[] args) {
-        int[] table = new int[]{3, 5,9, 11, 12};
+        int[] table = new int[]{3, 5, 9, 11, 12};
         byThreeDivisibilityValidation(table);
     }
 }
