60d5aaae2b05864d98d02f186d5596f01e10feb6
Piotr Białas <mrbialas123@gmail.com>
2022-10-01 19:10:34 +0200

    Exercise 8 Exceptions: Poprawa

--- Bootcamp/src/main/java/com/futurecollars/lesson10/exercise8/Elemental.java
+++ Bootcamp/src/main/java/com/futurecollars/lesson10/exercise8/Elemental.java
@@ -1,7 +1,7 @@
 package com.futurecollars.lesson10.exercise8;
 
 public class Elemental {
-    public static double doElemental(double number) {
+    public double doElemental(double number) {
         if (number < 0) {
             throw new IllegalArgumentException("Liczba nie może być mniejsza od 0");
         }
--- Bootcamp/src/main/java/com/futurecollars/lesson10/exercise8/Main.java
+++ Bootcamp/src/main/java/com/futurecollars/lesson10/exercise8/Main.java
@@ -2,12 +2,13 @@ package com.futurecollars.lesson10.exercise8;
 
 import java.util.Scanner;
 
-public class Main extends Elemental {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Wprowadź liczbę nieujemną do obliczenia jej pierwiastka");
         Scanner sc = new Scanner(System.in);
         double number = sc.nextInt();
-        double answer = doElemental(number);
+        Elemental elemental = new Elemental();
+        double answer = elemental.doElemental(number);
         if (answer != 0) {
             System.out.println("Rozwiązaniami są: " + answer + " oraz " + -answer);
         } else {
