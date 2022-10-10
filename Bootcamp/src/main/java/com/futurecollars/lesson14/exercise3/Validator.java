1c4716d1abed5332bfc6e6ca2a64f48acb1b0e01
Laptopik <mrbialas@wp.pl>
2022-10-10 13:01:49 +0200

    Exercise 3 Regex: Poprawa

--- Bootcamp/src/main/java/com/futurecollars/lesson14/exercise3/Validator.java
+++ Bootcamp/src/main/java/com/futurecollars/lesson14/exercise3/Validator.java
@@ -5,18 +5,8 @@ import java.util.regex.Pattern;
 
 public class Validator {
     public boolean validateUrlLink(String link) {
-        Pattern pattern1 = Pattern.compile("^https://");
-        Pattern pattern2 = Pattern.compile("^http://");
-        Pattern pattern3 = Pattern.compile(".*\\.com");
-        Pattern pattern4 = Pattern.compile(".*\\.pl");
-        Matcher matcher1 = pattern1.matcher(link);
-        Matcher matcher2 = pattern2.matcher(link);
-        Matcher matcher3 = pattern3.matcher(link);
-        Matcher matcher4 = pattern4.matcher(link);
-        if (matcher1.find() || matcher2.find()) {
-            return (matcher3.find() || matcher4.find());
-        } else {
-            return false;
-        }
+        Pattern pattern = Pattern.compile("^https?://.\\.*");
+        Matcher matcher = pattern.matcher(link);
+        return matcher.find();
     }
 }
